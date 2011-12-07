d4c605fa57c29c85ea4c481a17272759f5c3f4f5
Rob Ashton <robashton@codeofrob.com>
2011-12-07 21:45:19 +0100

    Added a build script (temporary while I think about packaging) and moved things to demos folder - think I need to work out how to remove jQuery

---
+++ build.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+
+var inputFiles = [
+  "src/camera.js",
+  "src/rendertarget.js" ,
+  "src/effectbuilder.js" ,
+  "src/effect.js" ,
+  "src/material.js" ,
+  "src/canvasrenderstage.js" ,
+  "src/webglrenderer.js" ,
+  "src/webglrenderstage.js" ,
+  "src/layer.js" ,
+  "src/world.js" ,
+  "src/game.js"
+];
+
+// TODO: Discuss strategy for dealing with modules in a purely client-side system
+// it is tempted to go down the requireJS route and provide an entry point to the outside world that keeps them unaware of this
+// I'll think about it - for now there are only one way dependencies and this is nice and simple and works
+var outputText = '';
+
+var addFile = function(index, callback) {
+  console.log(inputFiles[index]);
+  fs.readFile(inputFiles[index], function(err, data) {
+    
+    outputText += data;
+    outputText += '\r\n';
+    
+    if(index < inputFiles.length-1)
+      addFile(index+1, callback);
+    else
+      callback();
+  });
+};
+
+var buildAllFiles = function(callback) {
+  addFile(0, callback);
+};
+
+
+buildAllFiles(function() {
+  fs.writeFile('lib/layers.js', outputText);
+});
